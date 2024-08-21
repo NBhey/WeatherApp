61ad388e0a88a9f8d58f18e82a810a21b626812c
Oganes <og.marckaryan@yandex.ru>
2024-08-21 21:41:38 +0300

    correct new settings in weather API step-2

--- src/weather.js
+++ src/weather.js
@@ -8,14 +8,19 @@ export async function getWeatherAndIcon(city) {
     // console.log(responseCity.results[0].latitude)
     // console.log(responseCity.results[0].longitude)
     // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${responseCity.results[0].latitude}&lon=${responseCity.results[0].longitude}&appid=${API_KEY}`;
-    const urlCity = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`;
-    const resCity = await fetch(urlCity);
+
+    const resCity = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`);
+    console.log(resCity);
+    const parseResCity = await resCity.json();
+    console.log(parseResCity);
+    console.log(parseResCity[0].lat);
+    console.log(parseResCity[0].lon);
     // console.log(resCity)
     // const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${API_KEY}&lang=ru}`;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${resCity.latitude}&lon=${resCity.longitude}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${parseResCity[0].lat}&lon=${parseResCity[0].lon}&appid=${API_KEY}&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data)
+    console.log(data)
     return [Math.round(data.main.temp), data.weather[0].icon, data.coord];
   } catch (err) {
     return;
